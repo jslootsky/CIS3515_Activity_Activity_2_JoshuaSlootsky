d92f7fd70213bc7dbc8d4a0c2011ebdc1614cd80
jslootsky <joshuaslootsky@gmail.com>
2025-02-20 11:51:04 -0500

    implemented step 1. instantiated and registered the launcher that will modify the text size of the text view

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -1,13 +1,33 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+
+const val TEXT_SIZE_KEY = "text size"
 
 class DisplayActivity : AppCompatActivity() {
 
-    // TODO Step 1: Launch TextSizeActivity when button clicked to allow selection of text size value
+    // TODO Step 1: Launch TextSizeActivity when button clicked to
+    //  allow selection of text size value
+    //create an instance of a launcher, used to start the activity
+    //registerForActivity registers the launcher and specifies the type of contract
+    //ActivityResultsContact.StartActivityForResult
+        //type of contract that tells the launcher it will start an activity
+        //and expect a result
+    //lambda function inside runs upon the conclusion of the activity
+        //checks the result code, if OK, modifies the text
+    val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if(it.resultCode == RESULT_OK){
+            val newSize = it.data?.getFloatExtra(TEXT_SIZE_KEY, 22f)
+            if (newSize != null) {
+                lyricsDisplayTextView.textSize = newSize
+            }
+        }
+    }
 
     // TODO Step 3: Use returned value for lyricsDisplayTextView text size
 
